98fe5158d10cba0605a5c967d7237d56c105e484
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-02-28 13:55:38 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = math.floor(math.ranodm() * 1000) + 1;
+const num3 = 24;
+const num4 = 10;
+const mod = num3 % num4;
+const max = math.max(5,10,15,20)
